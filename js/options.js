c252cdff2a1e84b28987f3a2f27d3c2938f1b723
Nilanjan <neelpulse@gmail.com>
2013-02-13 06:35:13 +0530

    Username and password functionality

--- js/options.js
+++ js/options.js
@@ -1,15 +1,27 @@
 $(function() {
+    //~ $("#notification_bar").fadeIn(30).fadeOut(3000);
+    
+    function notify(msg) {
+        $("#notification_bar").text(msg);
+        $("#cont").fadeIn(30).delay(3000).fadeOut(300); //notify
+    }    
     
     $("#save").on("click", function(){
         var username = $("#uname").val();
         var password = $("#passwd").val();
         var password_rep = $("#passwd_rep").val();
         
-        alert(username + password + password_rep);
-        if( (username && password && password_rep) && ( password == password_rep) ) {
-            alert("Validated");
+        //~ alert(username + password + password_rep);
+        if(username && password && password_rep){ 
+            if( password == password_rep)  {
+                localStorage["plivoUsername"] = username;
+                localStorage["plivoPassword"] = password;
+                notify("Saved Successfully");
+            } else {
+                notify("Passwords do not match. Please try again");                
+            }
         } else {
-            return false;
+            notify("Please fill all the fields");
         }
     });
     
